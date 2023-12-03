f286209ef88b7539de84858c471dbaa44c817c13
mhmadamrii <interceptorghost4@gmail.com>
2023-12-03 18:06:20 +0700

    add evjkdas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+import Testing from '@/components/testing';
+
+const handlePost = async () => {
+  try {
+    const data = await fetch('/api/todos', { method: 'POST' });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/* 
+ *  testing doang
+ *
+ * */
+
+
+
+export default async function Home() {
+  const data = await handlePost();
+  console.log(data);
   return (
     <div>
       <h1>homescreen</h1>
+      <Testing />
     </div>
   );
 }
---
+++ components/testing.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Testing() {
+  const handleGet = async () => {};
+  return (
+    <div>
+      <h1 className="text-sm">Hello world</h1>
+    </div>
+  );
+}
